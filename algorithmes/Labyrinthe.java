16843af26d5288574de89a7c385b21da4b121d5f
unknown <alexandre.bennour4@etu.univ-lorraine.fr>
2019-01-03 20:36:30 +0100

    ajjout lab

---
+++ algorithmes/Labyrinthe.java
@@ -0,0 +1,27 @@
+package algorithmes;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import structure.Edge;
+import structure.Graph;
+
+public class Labyrinthe {
+	
+	private Graph base;
+	
+	public Labyrinthe(Algorithme al) {
+		this.base = al.executer(Graph.G2());
+	}
+	
+	public BufferedImage toImage(){
+		return base.toLabyrinthe();
+	}
+	
+	
+	
+	
+	
+}
